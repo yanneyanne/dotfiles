bf345bf7505f6a50d1f5caf91ceac932bd6ada75
Erik <erikejan@hotmail.com>
2019-08-13 14:20:59 +0200

    Install zsh, tmux and neovim through install script

--- install.sh
+++ install.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 ##########################
-# this install script creates symlinks from the home directory to the dotfiles in ~/dotfiles
+# This install script:
+# * makes sure that zsh, tmux and neovim are installed correctly
+# * creates symlinks from the home directory to the dotfiles in ~/dotfiles
+##########################
+
+##########################
+# Install dependencies
+##########################
+
+if [ -z $(which zsh)]; then
+	echo "zsh not found. Installing zsh..."
+	brew install zsh
+	echo "Making zsh default shell"
+	chsh -s $(which zsh)
+fi
+
+if [ -z $(which nvim)]; then
+	echo "neovim not found. Installing neovim..."
+	brew install neovim
+	echo "Installing python3 and python-for-neovim"
+	brew install python3
+	pip2 install neovim --upgrade
+	pip3 install neovim --upgrade
+	echo "Installing neovim plugins"
+	vim +'PlugInstall --sync' +qa
+fi
+
+if [ -z $(which tmux)]; then
+	echo "tmux not found. Installing tmux..."
+	brew install tmux
+fi
+
+
+##########################
+# Create symlinks for dotfiles
 ##########################
 
 dir=~/dotfiles
